1cf66a102e60e4ac846e39f119b7b8960b775e66
Che Hee Seok <dswill308@gmail.com>
2020-07-03 04:43:35 +0900

    새로 만든 파일들이 커밋이 안 돼서 다시 커밋합니다.

---
+++ CubePang/Assets/Scripts/Managers/SceneChangeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager : MonoBehaviour
+{
+    static SceneChangeManager Instance = null;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadMainGameScene()
+    {
+        SceneManager.LoadSceneAsync("MainGameScene");
+    }
+
+    public void LoadLobbyScene()
+    {
+        SceneManager.LoadSceneAsync("LobbyScene");
+    }
+
+    public void QuitScene()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+}
---
+++ CubePang/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioSource cubeSource;
+    public AudioSource explosionSource;
+
+    public static SoundManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlayCubeSound( )
+    {
+        if (false == cubeSource.isPlaying)
+        {
+            cubeSource.pitch = Random.Range(0.95f, 1.05f);
+            cubeSource.Play();
+        }
+    }
+
+    public void PlayExplosionSound()
+    {
+        explosionSource.pitch = Random.Range(0.95f, 1.05f);
+        explosionSource.Play();
+    }
+}
